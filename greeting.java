a3c9a951f02f81148db813df342ea62f00ef73e8
vikassharma18 <vikas.sh1802@gmail.com>
2024-11-25 08:01:58 +0530

    greating

---
+++ greeting.java
@@ -0,0 +1,11 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class greeting {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("Hello, how are you "+name);
+    }
+}
